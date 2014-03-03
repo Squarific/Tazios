18608c7feb249586c5da35fba722468abd6d291a
Filip Smets <alemaaltevinden@gmail.com>
2014-03-03 20:17:16 +0100

    Read width/height from canvas not ctx

--- client/js/SQ.Screen.js
+++ client/js/SQ.Screen.js
@@ -16,8 +16,8 @@ SQ.Screen.prototype.drawLayer = function drawLayer (startX, startY, layerdata, l
 	if (!targetCtx) {
 		targetCtx = this.ctx;
 	}
-	var maxX = Math.floor((startX + targetCtx.width) / tileWidth),
-		maxY = Math.floor((startY + targetCtx.height) / tileHeight);
+	var maxX = Math.floor((startX + targetCtx.canvas.width) / tileWidth),
+		maxY = Math.floor((startY + targetCtx.canvas.height) / tileHeight);
 	for (var x = Math.floor(startX / tileWidth); x < maxX; x++) {
 		for (var y = Math.floor(startY / tileHeight); y < maxY; y++) {
 			var tileNumber = Math.min(x, 0) + Math.min(y, 0) * layerdatawidth;
