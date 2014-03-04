ae3bab07fa082e8e883745e23507095fc371cfe9
Filip Smets <alemaaltevinden@gmail.com>
2014-03-04 11:06:29 +0100

    Added SQ.InputHAndler file

---
+++ client/js/SQ.InputHandler.js
@@ -0,0 +1,17 @@
+var SQ = SQ || {};
+
+SQ.InputHandler = function InputHandler () {
+	
+};
+
+SQ.InputHandler.prototype.addKeypressListeners = function keyPressed (listeners) {
+	
+};
+
+SQ.InputHandler.prototype.addKeyupListeners = function keyPressed (listeners) {
+	
+};
+
+SQ.InputHandler.prototype.adKeydownListeners = function keyPressed (listeners) {
+	
+};
